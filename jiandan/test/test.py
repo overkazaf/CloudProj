f8f6ce217dbf77a897bf07493ff26e2e80b80f30
John Mitnick <overkazaf@gmail.com>
2016-04-24 23:10:10 +0800

    add theme demo

---
+++ jiandan/test/test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+import requests
+from bs4 import BeautifulSoup
+
+res = requests.get('http://jiandan.net/ooxx')
+html = BeautifulSoup(res.text) 
+for index, each in enumerate(html.select('#comments img')):
+	with open('{}.jpg'.format(index), 'wb') as jpg:
+		jpg.write(requests.get(each.attrs['src'], stream=True).content)
+
